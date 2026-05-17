5a1afcc451be86da37905ff40852f60c5ca3145c
ProcessHandle <146387532+ProcessHandle@users.noreply.github.com>
2026-05-17 04:42:24 -0400

    Update cur.lua

--- cur.lua
+++ cur.lua
@@ -1,53 +1,63 @@
-local retardremote = nil
+local communicator = require(game:GetService("ReplicatedStorage"):FindFirstChild("Libraries").Communicator)
 
+-- Search through all functions in communicator for the one that calls InvokeServer
+for k, v in pairs(communicator) do
+    if type(v) == "function" then
+        local upvalues = {}
+        local i = 1
+        while true do
+            local name, value = debug.getupvalue(v, i)
+            if not name then break end
+            if type(value) == "function" then
+                table.insert(upvalues, {name = name, func = value})
+            end
+            i = i + 1
+        end
+        if #upvalues > 0 then
+            print("Function " .. k .. " has " .. #upvalues .. " upvalues")
+        end
+    end
+end
+
+-- Hook the actual remote directly using the method that worked before
+local reincRemote = nil
 for _, folder in pairs(game:GetDescendants()) do
     if folder.Name == "FilteredSelection" and #folder:GetChildren() > 0 then
         for _, v in pairs(folder:GetChildren()) do
             if v:IsA("RemoteFunction") then
-                retardremote = v
+                reincRemote = v
                 break
             end
         end
     end
 end
 
-if retardremote then
-    local mt = getrawmetatable(retardremote)
-    if mt then
-        local oldNamecall = mt.__namecall
-        setreadonly(mt, false)
-        mt.__namecall = function(self, ...)
-            local method = getnamecallmethod()
-            if method == "InvokeServer" then
-                local args = {...}
-                print("[InvokeServer] Args:")
-                for i, arg in pairs(args) do
-                    if type(arg) == "buffer" then
-                        print("  Arg[" .. i .. "] is a buffer of size: " .. buffer.len(arg))
-                        pcall(function()
-                            local str = buffer.tostring(arg)
-                            print("    Buffer as string: " .. str)
-                        end)
-                        local hex = ""
-                        for j = 0, math.min(31, buffer.len(arg)-1) do
-                            hex = hex .. string.format("%02x", buffer.readu8(arg, j))
-                        end
-                        print("    Hex (first 32 bytes): " .. hex)
-                    elseif type(arg) == "table" then
-                        print("  Arg[" .. i .. "] is a table with contents:")
-                        for k, v in pairs(arg) do
-                            print("    " .. tostring(k) .. " = " .. tostring(v))
-                        end
-                    else
-                        print("  Arg[" .. i .. "] = " .. tostring(arg) .. " (" .. type(arg) .. ")")
-                    end
+if reincRemote then
+    print("Remote found, tracking calls")
+    local mt = getrawmetatable(reincRemote)
+    local oldNamecall = mt.__namecall
+    setreadonly(mt, false)
+    mt.__namecall = function(self, ...)
+        local method = getnamecallmethod()
+        if method == "InvokeServer" and self == reincRemote then
+            local args = {...}
+            print("=== REINCARNATION INVOKE ===")
+            for i, arg in pairs(args) do
+                if type(arg) == "buffer" then
+                    print("Arg[" .. i .. "] is buffer, size: " .. buffer.len(arg))
+                    -- Try to read as different types
+                    pcall(function()
+                        print("  As string: " .. buffer.tostring(arg))
+                    end)
+                else
+                    print("Arg[" .. i .. "]:", arg, "(" .. type(arg) .. ")")
                 end
             end
-            return oldNamecall and oldNamecall(self, ...) or nil
+            print("Stack trace:")
+            print(debug.traceback())
         end
-        setreadonly(mt, true)
-        print("Hooked - will show buffer contents")
-    else
-        print("No metatable found")
+        return oldNamecall(self, ...)
     end
+    setreadonly(mt, true)
+    print("Hook ready - trigger reincarnation")
 end

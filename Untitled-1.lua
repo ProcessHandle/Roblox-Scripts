432f3638a0e61fffc585a06bfcadd4c40dbd9a9f
ProcessHandle <146387532+ProcessHandle@users.noreply.github.com>
2026-09-20 16:49:57 -0400

    Update Untitled-1.lua

--- Untitled-1.lua
+++ Untitled-1.lua
@@ -3,7 +3,7 @@ local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
 
 local TARGETS = {
-    GloEgg = true, HarvestEgg = true, FarmEgg = true,
+    GloEgg = true, HarvestEgg = true, FarmEgg = true, BattleEgg = true,
     BigGloEgg = true, BigHarvestEgg = true, BigFarmEgg = true
 }
 
